e637ff15a68813a43819c95cec18f9236bc384c8
Hoon Wee <mrgravity817@gmail.com>
2023-07-13 19:24:54 +0900

    feat: improve with stream

--- lib/airports.ex
+++ lib/airports.ex
@@ -12,6 +12,10 @@ defmodule Airports do
     Application.app_dir(:airports, "/priv/airports.csv")
   end
 
+  @doc """
+  Opens airports data at once - uploading on CPU.
+  This might be slower than stream_airports/0.
+  """
   def open_airports() do
     airports_csv()
     |> File.read!()
@@ -25,5 +29,24 @@ defmodule Airports do
       }
     end)
     |> Enum.reject(&(&1.type == "closed"))
+  end
+
+  @doc """
+  Stream (lazily open) airports data.
+  """
+  def stream_airports() do
+    airports_csv()
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Stream.map(fn row ->
+      %{
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
+      }
+    end)
+    |> Stream.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
   end
 end
