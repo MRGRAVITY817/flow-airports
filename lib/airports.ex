b982b68c42eb5cf51e26bffe3ddb1189fd5c523e
Hoon Wee <mrgravity817@gmail.com>
2023-07-15 00:15:05 +0900

    feat: process continuous stream of data

--- lib/airports.ex
+++ lib/airports.ex
@@ -77,4 +77,48 @@ defmodule Airports do
     |> Enum.to_list()
     |> List.flatten()
   end
+
+  @doc """
+  continuously stream airport
+  """
+  def cont_airport() do
+    window = Flow.Window.trigger_every(Flow.Window.global(), 1000)
+
+    airports_csv()
+    |> File.stream!()
+    |> Stream.map(fn event ->
+      Process.sleep(Enum.random([0, 0, 0, 1]))
+      event
+    end)
+    |> Flow.from_enumerable()
+    |> Flow.map(fn row ->
+      [row] = CSV.parse_string(row, skip_headers: false)
+
+      %{
+        id: Enum.at(row, 0),
+        type: Enum.at(row, 2),
+        name: Enum.at(row, 3),
+        country: Enum.at(row, 8)
+      }
+    end)
+    |> Flow.reject(&(&1.type == "closed"))
+    |> Flow.partition(window: window, key: {:key, :country})
+    |> Flow.group_by(& &1.country)
+    |> Flow.on_trigger(fn acc, _partition_info, {_type, _id, trigger} ->
+      # Show progress in IEx, or use the data for something else
+      events =
+        acc
+        |> Enum.map(fn {country, data} -> {country, Enum.count(data)} end)
+        |> IO.inspect(label: inspect(self()))
+
+      case trigger do
+        :done -> {events, acc}
+        # don't emit anything since we're not finished processing
+        {:every, 1000} -> {[], acc}
+      end
+    end)
+    |> Flow.take_sort(10, fn {_, a}, {_, b} -> a > b end)
+    |> Enum.to_list()
+    |> List.flatten()
+  end
 end
